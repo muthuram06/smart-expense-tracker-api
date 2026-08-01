eabb0e5e0f10269f4a97cfc512f86359c43e93b3
muthuram06 <muthuram.6565@gmail.com>
2026-08-01 13:35:13 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/diligent/smart_expense_tracker_api/SmartExpenseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diligent.smart_expense_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExpenseTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
